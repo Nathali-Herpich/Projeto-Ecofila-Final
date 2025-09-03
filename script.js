41273f1776649943e5b16f6433b85a5d44685ce4
Nathali Herpich Valle <nathaliherpichvalle@gmail.com>
2025-09-03 10:09:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function abrirInstagram() {
+  window.open("https://www.instagram.com/ecofila_filamentos/?hl=en", "_blank");
+}
+function abrirInstagram2(){
+  window.open("https://www.instagram.com/soucesmar/?hl=en", "_blank");
+}
